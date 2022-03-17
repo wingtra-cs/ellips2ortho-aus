8c73efea5e450b5cd027b5c2072facc6d5e70568
irwin-amago <84965828+irwin-amago@users.noreply.github.com>
2022-03-17 15:39:05 +0800

    Update ellips2ortho_aus_app.py

--- ellips2ortho_aus_app.py
+++ ellips2ortho_aus_app.py
@@ -1,24 +1,112 @@
 import pandas as pd
-import requests
+import math
+import numpy
+import rasterio.sample
 import streamlit as st
-import pydeck as pdk
 
+geoid09_file = r"C:\Users\irwin\OneDrive\Desktop\Ellips2Ortho\au_ga_AUSGeoid09_V1.01.tif"
+geoid20_file = r"C:\Users\irwin\OneDrive\Desktop\Ellips2Ortho\au_ga_AUSGeoid2020_20170908.tif"
 
-st.title('Ellipsoidal to Orthometric Heights')
-st.caption('The application uses the NGS Geoid API to look up the geoid height at a particular location and uses this value to then compute the orthometric height based on the desired units of the user.')
+def geo_to_cart(lat, lon, h):
+    lat_rad = (lat/180)*math.pi
+    lon_rad = (lon/180)*math.pi
+    
+    a = 6378137.00
+    f = 1/298.257222101
+    
+    e2 = 2*f - f*f
+    v = a/(math.sqrt(1 - e2*math.sin(lat_rad)*math.sin(lat_rad)))
+
+    x = (h + v)*math.cos(lat_rad)*math.cos(lon_rad)
+    y = (h + v)*math.cos(lat_rad)*math.sin(lon_rad)
+    z = ((1 - e2)*v + h)*math.sin(lat_rad)
+    
+    return x,y,z
+
+def cart_to_cart(x0, y0, z0):
+    Tx = 0.06155
+    Ty = -0.01087
+    Tz = -0.04019
+    Sc = -0.009994
+    Rx = -0.0394924
+    Ry = -0.0327221
+    Rz = -0.0328979
+    
+    RxRad = ((Rx/3600)/180)*math.pi
+    RyRad = ((Ry/3600)/180)*math.pi
+    RzRad = ((Rz/3600)/180)*math.pi
+    Scale = 1 + (Sc/1000000)
+    
+    T = numpy.zeros(shape=(3,1))
+    T[0][0] = Tx
+    T[1][0] = Ty
+    T[2][0] = Tz
+    
+    R = numpy.zeros(shape=(3,3))
+    R[0][0] = math.cos(RyRad)*math.cos(RzRad)
+    R[0][1] = math.cos(RyRad)*math.sin(RzRad)
+    R[0][2] = -math.sin(RyRad)
+    
+    R[1][0] = math.sin(RxRad)*math.sin(RyRad)*math.cos(RzRad) - math.cos(RxRad)*math.sin(RzRad) 
+    R[1][1] = math.sin(RxRad)*math.sin(RyRad)*math.sin(RzRad) + math.cos(RxRad)*math.cos(RzRad) 
+    R[1][2] = math.sin(RxRad)*math.cos(RyRad)
+
+    R[2][0] = math.cos(RxRad)*math.sin(RyRad)*math.cos(RzRad) + math.sin(RxRad)*math.sin(RzRad) 
+    R[2][1] = math.cos(RxRad)*math.sin(RyRad)*math.sin(RzRad) - math.sin(RxRad)*math.cos(RzRad) 
+    R[2][2] = math.cos(RxRad)*math.cos(RyRad)
+    
+    Xold = numpy.zeros(shape=(3,1))
+    Xold[0][0] = x0
+    Xold[1][0] = y0
+    Xold[2][0] = z0
+
+    Xnew = T + Scale*numpy.matmul(R, Xold)
+    
+    return Xnew[0][0], Xnew[1][0], Xnew[2][0]
+
+def cart_to_geo(x1, y1, z1):
+    a = 6378137.00
+    f = 1/298.257222101
+    
+    e2 = 2*f - f*f
+    p = math.sqrt(x1*x1 + y1*y1)
+    r = math.sqrt(p*p + z1*z1)
+    u = math.atan((z1/p)*((1-f) + (e2*a)/r))
+    
+    lat_top = z1*(1-f) + e2*a*math.sin(u)*math.sin(u)*math.sin(u)
+    lat_bot = (1-f)*(p - e2*a*math.cos(u)*math.cos(u)*math.cos(u))
+    
+    lon_rad = math.atan(y1/x1)
+    if lon_rad < 0:
+        lon = 180*(math.pi + lon_rad)/math.pi
+    else:
+        lon = 180*lon_rad/math.pi
+    
+    lat_rad = math.atan(lat_top/lat_bot)
+    lat = 180*lat_rad/math.pi
+    
+    h = p*math.cos(lat_rad) + z1*math.sin(lat_rad) - a*math.sqrt(1 - e2*math.sin(lat_rad)*math.sin(lat_rad))
+
+    return lat, lon, h
+
+def gda94_to_gda2020(lat, lon, h):
+    x, y, z = geo_to_cart(lat, lon, h)
+    x1, y1, z1 = cart_to_cart(x, y, z)
+    
+    return cart_to_geo(x1, y1, z1)
 
 uploaded_csv = st.file_uploader('Please Select Geotags CSV.')
-
 if uploaded_csv is not None:
     df = pd.read_csv(uploaded_csv, index_col=False)
     msg = 'Upload Successful: ' + uploaded_csv.name
     st.success(msg)
 
+    geoid09_file = './au_ga_AUSGeoid09_V1.01.tif'
+    geoid20_file = './au_ga_AUSGeoid2020_20170908.tif'
 
     lat = 'latitude [decimal degrees]'
     lon = 'longitude [decimal degrees]'
     height = 'altitude [meter]'
-    
     
     points_df = pd.concat([df[lat], df[lon]], axis=1, keys=['lat','lon'])
     
@@ -41,105 +129,71 @@ if uploaded_csv is not None:
          ],
      ))
 
+    # Geoid Selection
 
-    # Select Geoid Model
-
-    geoid_dict = {'GEOID99': 1,
-                  'G99SSS': 2,
-                  'GEOID03': 3,
-                  'USGG2003': 4,
-                  'GEOID06': 5,
-                  'USGG2009': 6,
-                  'GEOID09': 7,
-                  'The latest experimental Geoid (XUSHG)': 9,
-                  'USGG2012': 11,
-                  'GEOID12A': 12,
-                  'GEOID12B': 13,
-                  'GEOID18': 14}
-    units_dict ={'Meters': 1, 'US Feet': 2}
-    
     geoid_select = st.selectbox('Please Choose Desired Geoid', ('<select>',
-                                                                'GEOID99',
-                                                                'G99SSS',
-                                                                'GEOID03',
-                                                                'USGG2003',
-                                                                'GEOID06',
-                                                                'USGG2009',
-                                                                'GEOID09',
-                                                                'The latest experimental Geoid (XUSHG)',
-                                                                'USGG2012',
-                                                                'GEOID12A',
-                                                                'GEOID12B',
-                                                                'GEOID18'))
-    
+                                                                'AusGeoid09',
+                                                                'AusGeoid20',))
+
     if not geoid_select=='<select>':
         st.write('You selected:', geoid_select)
-        geoid = geoid_dict[geoid_select]
-    
-    units_select = st.selectbox('Please Select Desired Units', ('<select>', 'Meters','US Feet'))
-    
-    if not units_select=='<select>':
-        st.write('You selected:', units_select)
-        units = units_dict[units_select]
-    
-    if uploaded_csv is not None and not geoid_select=='<select>' and not units_select=='<select>':
+
+    if uploaded_csv is not None and not geoid_select=='<select>':
         if st.button('CONVERT HEIGHTS'):
-            my_bar = st.progress(0)
-            cmd = 'https://geodesy.noaa.gov/api/geoid/ght?'
-            
-            ortho = []
-            for x in range(len(df[lat])):
-                lat_req = str(df[lat][x])
-                lon_req = str(df[lon][x])
-                ellip = df[height][x]
-                req = cmd + 'lat=' + lat_req + '&lon=' + lon_req + '&model=' + str(geoid)
-                    
-                try:
-                    responseGeoid = requests.get(req)
-                    responseGeoid.raise_for_status()
-                except requests.HTTPError  as errh:
-                    msg = "Error Connecting: " + str(errh)
-                    st.error(msg)
-                    st.stop()
-                except requests.ConnectionError as errc:
-                    msg = "Error Connecting: " + str(errc)
-                    st.error(msg)
-                    st.stop()
-                except requests.Timeout as errt:
-                    msg = "Error Connecting: " + str(errt)
-                    st.error(msg)
-                    st.stop()
-                except requests.RequestException as err:
-                    msg = "Error Connecting: " + str(err)
-                    st.error(msg)
-                    st.stop()
-            
-                my_bar.progress((x+1)/len(df[lat]))     
-                ortho_height = ellip - responseGeoid.json()['geoidHeight']
-                    
-                if units==1:
-                    ortho.append(ortho_height)
-                else:
-                    ortho.append(ortho_height*3.2808399)
-    
-            df[height] = ortho
-            if units==1:
-                df.rename(columns={height: 'orthometric height [meter]'}, inplace=True)
+
+            if geoid_select=='AusGeoid09':
+                ortho = []
+                geoid09 = rasterio.open(geoid09_file)
+                points = list(zip(df[lon].tolist(), df[lat].tolist()))
+
+                i = 0
+                for val in geoid09.sample(points):
+                    ortho.append(df[height][i] - val[0])
+                    i += 1
+
+                df[height] = ortho
+                df.rename(columns={lat: 'latitude GDA94 [decimal degrees]',
+                                   lon: 'longitude GDA94 [decimal degrees]',
+                                   height: 'orthometric height AusGeoid09 [meters]'}, inplace=True)
+
             else:
-                df['accuracy horizontal [meter]'] = df['accuracy horizontal [meter]'].apply(lambda x: x*3.2808399)
-                df['accuracy vertical [meter]'] = df['accuracy vertical [meter]'].apply(lambda x: x*3.2808399)
-                df.rename(columns={height: 'orthometric height [feet]',
-                                   'accuracy horizontal [meter]': 'accuracy horizontal [feet]', 
-                                   'accuracy vertical [meter]': 'accuracy vertical [feet]'}, inplace=True)
-            
+                ortho = []
+                geoid20 = rasterio.open(geoid20_file)
+
+                # Convert Coordinates
+                lat_gda20 = []
+                lon_gda20 = []
+                h_gda20 = []
+
+                for x in range(len(df[lat])):
+                    la, lo, h = gda94_to_gda2020(df[lat][x], df[lon][x], df[height][x])
+                    lat_gda20.append(la)
+                    lon_gda20.append(lo)
+                    h_gda20.append(h)
+
+                points = list(zip(lon_gda20,lat_gda20))
+
+                i = 0
+                for val in geoid20.sample(points):
+                    ortho.append(h_gda20[i] - val[0])
+                    i += 1
+
+                df[lat] = lat_gda20
+                df[lon] = lon_gda20
+                df[height] = ortho
+
+                df.rename(columns={lat: 'latitude GDA20 [decimal degrees]',
+                                   lon: 'longitude GDA20 [decimal degrees]',
+                                   height: 'orthometric height AusGeoid20 [meters]'}, inplace=True)
+
             st.success('Height conversion finished. Click button below to download new CSV.')
-            
+
             def convert_df(df):
                  return df.to_csv(index=False).encode('utf-8')
-            
+
             csv = convert_df(df)
             filename = uploaded_csv.name.split('.')[0] + '_orthometric.csv'
-            
+
             st.download_button(
                  label="Download Converted Geotags CSV",
                  data=csv,
